070a84698b6b9b4e2285a7b3ddae643e4e37ec2a
Tom <senteswins@gmail.com>
2021-05-30 17:22:45 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
-
-
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
-  }  
+    return MaterialApp(
+        home: new Scaffold(
+      appBar: AppBar(
+        title: new Text('My App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text('Question'),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1'))
+        ],
+      ),
+    ));
+  }
 }
