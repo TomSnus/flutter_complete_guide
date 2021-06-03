d5d53b4d131b152df33a56c19dac7680e2e45979
Tom <senteswins@gmail.com>
2021-06-03 09:44:39 +0200

    add appstate

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,29 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('answer chosen');
-  }
+var questionIndex = 0;
 
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite animal?'
+    ];
+    void answerQuestion() {
+      setState(() {
+        questionIndex++;
+        print('answer chosen');
+      });
+    }
+
     return MaterialApp(
         home: new Scaffold(
       appBar: AppBar(
@@ -18,10 +34,10 @@ class MyApp extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Text('Question'),
-          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
-          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
-          ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1'))
+          Text(questions.elementAt(questionIndex)),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Blue')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Red')),
+          ElevatedButton(onPressed: answerQuestion, child: Text('Green'))
         ],
       ),
     ));
