a2d4b2c5c059b0f4bbf3ed1b130bd228e8690368
Tom <senteswins@gmail.com>
2021-06-05 11:16:27 +0200

    add questions

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/answer.dart';
 import 'package:flutter_complete_guide/question.dart';
 
+import 'quiz.dart';
+import 'result.dart';
+
 void main(List<String> args) {
   runApp(MyApp());
 }
@@ -16,43 +19,60 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var _totalScore = 0;
+  final _questions = const [
+    {
+      'questionText': 'What is your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'What is your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 3},
+        {'text': 'Lion', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'What  is your favorite number?',
+      'answers': [
+        {'text': '1', 'score': 10},
+        {'text': '2', 'score': 5},
+        {'text': '3', 'score': 3},
+        {'text': '4', 'score': 1}
+      ]
+    }
+  ];
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      _questionIndex++;
+      print('answer chosen');
+    });
+    if (_questionIndex < _questions.length) {
+      print('More questions');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What is your favorite color?',
-        'answers': ['red', 'blue', 'green']
-      },
-      {
-        'questionText': 'What is your favorite animal?',
-        'answers': ['dog', 'cat', 'bird']
-      },
-      {
-        'questionText': 'What  is your favorite number?',
-        'answers': ['1', '2', '3']
-      }
-    ];
-    void _answerQuestion() {
-      setState(() {
-        _questionIndex++;
-        print('answer chosen');
-      });
-    }
-
     return MaterialApp(
         home: new Scaffold(
-      appBar: AppBar(
-        title: new Text('My App'),
-      ),
-      body: Column(
-        children: <Widget>[
-          new Question(questions.elementAt(_questionIndex)['questionText']),
-          ...(questions[_questionIndex]['answers'] as List<String>)
-              .map((answer) {
-            return Answer(_answerQuestion, answer);
-          }).toList()
-        ],
-      ),
-    ));
+            appBar: AppBar(
+              title: new Text('My App'),
+            ),
+            body: _questionIndex < _questions.length
+                ? Quiz(
+                    questions: _questions,
+                    answerQuestion: _answerQuestion,
+                    questionIndex: _questionIndex)
+                : Result(_totalScore)));
   }
 }
